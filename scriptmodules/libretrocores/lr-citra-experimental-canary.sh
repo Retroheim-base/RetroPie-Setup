63da4ad3da72aaae77b790c5c688a0345fd3b0ba
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-13 21:53:44 -0500

    Update lr-citra-experimental-canary.sh

--- scriptmodules/libretrocores/lr-citra-experimental-canary.sh
+++ scriptmodules/libretrocores/lr-citra-experimental-canary.sh
@@ -21,7 +21,7 @@ function depends_lr-citra-experimental-canary() {
 }
 
 function sources_lr-citra-experimental-canary() {
-    gitPullOrClone "$md_build" https://github.com/retroheim/citra.git libretro
+    gitPullOrClone "$md_build" https://github.com/retroheim/citra.git
 }
 
 function build_lr-citra-experimental-canary() {
