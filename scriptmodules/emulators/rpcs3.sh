26ba7284aeabd9bce7d98b3d70b2b519eb167749
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-16 14:48:01 -0500

    Update rpcs3.sh
    https://github.com/raelgc/retropie_rpcs3/blob/master/rpcs3.sh

--- scriptmodules/emulators/rpcs3.sh
+++ scriptmodules/emulators/rpcs3.sh
@@ -11,26 +11,20 @@
 
 rp_module_id="rpcs3"
 rp_module_desc="PS3 emulator RPCS3"
-rp_module_help="ROM Extensions: .ps3\n\nCopy your PS3 roms to $romdir/ps3"
-rp_module_licence="GPL2 https://github.com/RPCS3/rpcs3/blob/master/LICENSE"
+rp_module_help="ROM Extensions: .ps3\n\nCopy your .PS3 game folders to $romdir/ps3\n\nDon't forget to run system firmware update first!"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/RPCS3/rpcs3/master/LICENSE"
 rp_module_section="exp"
 rp_module_flags="!arm"
 
 function install_bin_rpcs3() {
-    wget --content-disposition https://rpcs3.net/latest-appimage
-    rpcs3_bin=`ls rpcs3*AppImage`  
-    mv $rpcs3_bin $md_inst/$rpcs3_bin
-    chmod +x $md_inst/$rpcs3_bin
-    mkdir $romdir/ps3
-    touch $romdir/ps3/launch.ps3
+    mkdir -p "$md_inst/bin"
+    wget --content-disposition https://rpcs3.net/latest-appimage -O "$md_inst/bin/rpcs3.AppImage"
+    chmod +x "$md_inst/bin/rpcs3.AppImage"
 }
 
 function configure_rpcs3() {
     mkRomDir "ps3"
-    moveConfigDir "$home/.config/rpcs3" "$md_conf_root/ps3"
-
-    addEmulator 0 "$md_id-nogui" "ps3" "$md_inst/$rpcs3_bin %ROM% --nogui"
-    addEmulator 1 "$md_id" "ps3" "$md_inst/$rpcs3_bin"
-
+    addEmulator 0 "$md_id" "ps3" "$md_inst/bin/rpcs3.AppImage %ROM%/PS3_GAME/USRDIR/EBOOT.BIN"
+    addEmulator 1 "$md_id-nogui" "ps3" "$md_inst/bin/rpcs3.AppImage --no-gui %ROM%/PS3_GAME/USRDIR/EBOOT.BIN"
     addSystem "ps3"
 }
